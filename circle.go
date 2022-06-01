14e5b74956c3cc61309917ae68e7375c0ae79710
Anastasiia_Dremina <Anastasiia.Dremina@externals.adidas.com>
2022-06-01 23:15:03 +0300

    area + perimeter for shapes

--- circle.go
+++ circle.go
@@ -1,6 +1,15 @@
 package golang_united_school_homework
 
-// Circle must satisfy to Shape interface
+import "math"
+
 type Circle struct {
 	Radius float64
+}
+
+func (c Circle) CalcPerimeter() float64 {
+	return 2 * c.Radius * math.Pi
+}
+
+func (c Circle) CalcArea() float64 {
+	return math.Pi * math.Pow(2, c.Radius)
 }
--- reactangle.go
+++ reactangle.go
@@ -1,7 +1,13 @@
 package golang_united_school_homework
 
-// Rectangle must satisfy to Shape interface
 type Rectangle struct {
 	Height, Weight float64
 }
 
+func (r Rectangle) CalcPerimeter() float64 {
+	return 2 * (r.Weight + r.Height)
+}
+
+func (r Rectangle) CalcArea() float64 {
+	return r.Height * r.Weight
+}
